fadbfcb6dabd8d03d557a9cda7dc3f6e0f225c56
wenlit <wenlit@live.cn>
2018-08-08 21:44:21 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -16,15 +16,34 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-if(!auth.getToken('customerId')){
-	window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb77b53289e95c9e7&redirect_uri=http://zk302.s1.natapp.cc&response_type=code&scope=snsapi_base#wechat_redirect';
+//获取地址参数-code
+function GetQuery(name){
+	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r!=null)return  unescape(r[2]); return null;
+}
 
-	// service.enter({openid:'fsdfrger'}).then(rs => {
-	// 	if(rs.data.retCode=='000100'){
-	// 		auth.setToken('customerId',rs.data.customer.customerId);
-	// 		location.reload();
-	// 	}
-	// })
+
+if(!auth.getToken('customerId')){
+	var uri = window.location.href;
+	var arr = uri.split('?')
+	uri = arr[0];
+
+	var code = GetQuery("code");
+	if(code){
+		service.enter({code:code}).then(rs => {
+			if(rs.data.retCode=='000100'){
+				auth.setToken('customerId',rs.data.customer.customerId);
+				location.reload();
+			}
+		})
+	}else{
+		alert("请刷新页面")
+	}
+
+
+	window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb77b53289e95c9e7&redirect_uri='+uri+'&response_type=code&scope=snsapi_base#wechat_redirect';
+
 }
 var storage=window.localStorage;
 var sessionStorage=window.sessionStorage;
