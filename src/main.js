81eae7567e0f0c1533bb0f110ce18f5890945cc1
wenlit <wenlit@live.cn>
2018-08-16 14:21:15 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import router from './routes/router'
 import App from './App'
 import store from '@/store/index'
-import auth from './utils/auth'
+
 import axios from 'axios'
-import service from '@/services/services'
+
 Vue.config.productionTip = false
 
 
@@ -16,32 +16,9 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-//获取地址参数-code
-function GetQuery(name){
-	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-	var r = window.location.search.substr(1).match(reg);
-	if(r!=null)return  unescape(r[2]); return null;
-}
 
 
-if(!auth.getToken('customerId')){
-	var uri = window.location.href;
-	var arr = uri.split('?')
-	uri = arr[0];
-
-	var code = GetQuery("code");
-	if(code){
-		service.enter({code:code}).then(rs => {
-			if(rs.data.retCode=='000100'){
-				auth.setToken('customerId',rs.data.customer.customerId);
-				location.reload();
-			}
-		})
-	}else{
-		window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb77b53289e95c9e7&redirect_uri='+uri+'&response_type=code&scope=snsapi_base#wechat_redirect';
-	}
 
 
-}
 var storage=window.localStorage;
 var sessionStorage=window.sessionStorage;
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,6 @@
 import Axios from 'axios'
+import service from '@/services/services'
+import auth from './auth'
 
 const http = Axios.create({
     baseURL: '/appapi',
@@ -7,7 +9,37 @@ const http = Axios.create({
 })
 
 // 请求拦截器
+http.interceptors.request.use(request => {
+	if(!auth.getToken('customerId')){
+		var uri = window.location.href;
+		var arr = uri.split('?')
+		uri = arr[0];
 
+		var code = GetQuery("code");
+		if(code){
+			service.enter({code:code}).then(rs => {
+				if(rs.data.retCode=='000100'){
+				auth.setToken('customerId',rs.data.customer.customerId);
+				location.reload();
+			}
+		})
+	}else{
+		window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb77b53289e95c9e7&redirect_uri='+uri+'&response_type=code&scope=snsapi_base#wechat_redirect';
+	}
+
+
+}
+	return request;
+}, error => {
+	return Promise.reject(error);
+});
+
+//获取地址参数-code
+function GetQuery(name){
+	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r!=null)return  unescape(r[2]); return null;
+}
 
 // 响应拦截器
 
