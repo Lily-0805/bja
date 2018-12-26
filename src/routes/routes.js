e6b9e1e7412a03e8e318b5d82df2246a657eb051
Lily <tangli8805@163.com>
2018-12-26 23:06:35 +0800

    change title

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,12 @@
 const getViewComponent = (path) => (resolve) => require(['@/views' + path + '.vue'], resolve)
 
 export default [
-    {path: '/', component: getViewComponent('/index'),meta: {title: '保捷安物流'}},
+    {path: '/', component: getViewComponent('/index'),meta: {title: '吉丰物流'}},
 
-	{path: '/address/list', component: getViewComponent('/address/list'),meta: {title: '保捷安物流'}},
-	{path: '/address/add', component: getViewComponent('/address/add'),meta: {title: '保捷安物流'}},
+	{path: '/address/list', component: getViewComponent('/address/list'),meta: {title: '吉丰物流'}},
+	{path: '/address/add', component: getViewComponent('/address/add'),meta: {title: '吉丰物流'}},
 
-	{path: '/order/list', component: getViewComponent('/order/list'),meta: {title: '保捷安物流'}},
-	{path: '/express/detail', component: getViewComponent('/express/detail'),meta: {title: '保捷安物流'}},
+	{path: '/order/list', component: getViewComponent('/order/list'),meta: {title: '吉丰物流'}},
+	{path: '/express/detail', component: getViewComponent('/express/detail'),meta: {title: '吉丰物流'}},
 	{path: '/protocol', component: getViewComponent('/protocol'),meta: {title: '服务协议'}},
 ]
